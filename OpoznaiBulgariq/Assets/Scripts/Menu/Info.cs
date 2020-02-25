df91e882391e9e503c88a15e7b39c9b7ac87c8d1
petkopetkov987 <61232356+petkopetkov987@users.noreply.github.com>
2020-02-25 21:47:16 +0200

    Button fix

---
+++ OpoznaiBulgariq/Assets/Scripts/Menu/Info.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
+using UnityEngine.SceneManagement;
+
+public class Info : MonoBehaviour
+{
+    public void adasdsad ()
+    {
+        Application.LoadLevel(0);
+    }
+}
---
+++ OpoznaiBulgariq/Assets/Scripts/Questions/QuestionsManagerRaiskoPruskalo.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
+using UnityEngine.SceneManagement;
+
+public class QuestionsManagerRaiskoPruskalo : MonoBehaviour
+{
+    public Question[] questions;
+    private static List<Question> unansweredQuestions;
+
+    private Question currentQuestion;
+
+    [SerializeField]
+    private Text factText;
+
+    [SerializeField]
+    private Text trueAnswerText;
+
+    [SerializeField]
+    private Text falseAnswerText;
+
+    [SerializeField]
+    private Animator animator;
+
+    [SerializeField]
+    private float timeBetweenQuestions = 2f;
+
+    void Start()
+    {
+        if (unansweredQuestions == null || unansweredQuestions.Count == 0)
+        {
+            unansweredQuestions = questions.ToList<Question>();
+        }
+
+        SetCurrentQuestion();
+    }
+
+    void SetCurrentQuestion()
+    {
+        if (unansweredQuestions.Count == 0)
+        {
+            PlayAgain();
+            return;
+        }
+
+        int randomQuestionIndex = Random.Range(0, unansweredQuestions.Count);
+        currentQuestion = unansweredQuestions[randomQuestionIndex];
+
+        factText.text = currentQuestion.fact;
+
+        if (currentQuestion.isTrue)
+        {
+            trueAnswerText.text = "Правилен отговор!";
+            falseAnswerText.text = "Грешен отговор!";
+        }
+        else
+        {
+            trueAnswerText.text = "Грешен отговор!";
+            falseAnswerText.text = "Правилен отговор!";
+        }
+    }
+
+    IEnumerator TransitionToNextQuestion()
+    {
+        unansweredQuestions.Remove(currentQuestion);
+
+        yield return new WaitForSeconds(timeBetweenQuestions);
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void PlayAgain()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void UserSelectTrue()
+    {
+        animator.SetTrigger("True");
+
+        if (currentQuestion.isTrue)
+        {
+            Debug.Log("Правилен отговор!");
+        }
+        else
+        {
+            Debug.Log("Грешен отговор!");
+        }
+
+        StartCoroutine(TransitionToNextQuestion());
+    }
+
+
+    public void UserSelectFalse()
+    {
+        animator.SetTrigger("False");
+
+        if (!currentQuestion.isTrue)
+        {
+            Debug.Log("Правилен отговор!");
+        }
+        else
+        {
+            Debug.Log("Грешен отговор!");
+        }
+
+        StartCoroutine(TransitionToNextQuestion());
+    }
+
+}
